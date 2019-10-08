62d638783747cbe8c83d35af2817c558a2a3a4e6
Daisuke Matsuzaki <d@d.local>
2019-10-09 00:33:18 +0900

    Fix bug

--- sh/train_transfer.sh
+++ sh/train_transfer.sh
@@ -4,6 +4,6 @@ pipenv run python train.py\
     --name "cUNet_SNdisc_onlyGAN"\
     --pkl_path "/mnt/data2/matsuzaki/repo/data/sepalated_mini_data.pkl"\
     --classifier_path "cp/classifier/res_aug_5_cls/resnet101_95.pt"\
-    --lr 1e-5\
+    --lr 1e-4\
     --batch_size 16\
     --num_workers 8;
