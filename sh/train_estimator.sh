e9b54f10afe1634589ee91d5cd163e0b635321fb
Daisuke Matsuzaki <kgkgzrtk1030@gmail.com>
2019-11-19 20:55:57 +0900

    update sh

--- sh/train_estimator.sh
+++ sh/train_estimator.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-NAME=${1:-noname-classifier}
+NAME=${1:-noname-estimator}
+CUDA_DEVICE_ORDER="PCI_BUS_ID"
 CUDA_VISIBLE_DEVICES=${2:-0}
 pipenv run python estimator.py\
-    --pkl_path "/mnt/fs2/2018/matsuzaki/results/flickr_data/add_l2norm.pkl"\
+    --pkl_path "add_l2norm.pkl"\
     --save_path cp/classifier/${NAME}\
-    --batch_size 32;
+    --batch_size 16;
