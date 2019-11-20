31a88551643fe29cafd66dfb48f41365f1078f8b
kgkgzrtk <r111331@g.kobe-kosen.ac.jp>
2019-11-20 21:48:04 +0900

    Update sh

--- sh/train_estimator.sh
+++ sh/train_estimator.sh
@@ -3,8 +3,8 @@ NAME=${1:-noname-estimator}
 CUDA_DEVICE_ORDER="PCI_BUS_ID"
 CUDA_VISIBLE_DEVICES=${2:-0}
 pipenv run python estimator.py\
-    --pkl_path "/mnt/fs2/2018/matsuzaki/results/flickr_data/add_l2norm.pkl"\
+    --pkl_path "data_pkl/flickr_offset_under05.pkl"\
     --image_root "/mnt/fs2/2019/Takamuro/db/photos_usa_2016"\
     --save_path cp/estimator/${NAME}\
-    --num_worker 2\
+    --num_worker 4\
     --batch_size 16;
