00ede5a97eca2adac38b2f5de26636646794c1e7
Daisuke Matsuzaki <kgkgzrtk1030@gmail.com>
2019-11-20 22:34:17 +0900

    Update .env variables

--- sh/train_estimator.sh
+++ sh/train_estimator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NAME=${1:-noname-estimator}
-CUDA_DEVICE_ORDER="PCI_BUS_ID"
-CUDA_VISIBLE_DEVICES=${2:-0}
+echo CUDA_DEVICE_ORDER="PCI_BUS_ID" > .env
+echo CUDA_VISIBLE_DEVICES=${2:-0} >> .env
 pipenv run python estimator.py\
     --pkl_path "data_pkl/flickr_offset_under05.pkl"\
     --image_root "/mnt/fs2/2019/Takamuro/db/photos_usa_2016"\
     --save_path cp/estimator/${NAME}\
-    --num_worker 4\
+    --num_worker 8\
     --batch_size 16;
