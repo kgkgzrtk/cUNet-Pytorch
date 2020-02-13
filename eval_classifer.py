5fd391573a3e4f30a45f3d6746a67b1af45e1d01
kgkgzrtk <r111331@g.kobe-kosen.ac.jp>
2020-02-13 14:55:48 +0900

    Add eval script

---
+++ eval_classifer.py
@@ -0,0 +1,88 @@
+import argparse
+import pickle
+import os
+
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+from tqdm import trange, tqdm
+
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int)
+parser.add_argument('--image_root', type=str, default='/mnt/fs2/2018/matsuzaki/dataset_fromnitta/Image/')
+parser.add_argument('--pkl_path', type=str, default='/mnt/data2/matsuzaki/repo/data/sepalated_data.pkl')
+parser.add_argument('--output_dir', type=str, default='/mnt/fs2/2018/matsuzaki/results/eval/classifer')
+parser.add_argument('--classifer_path', type=str, default='cp/classifier/res_aug_5_cls/resnet101_95.pt')
+parser.add_argument('--input_size', type=int, default=224)
+parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('--num_workers', type=int, default=4)
+parser.add_argument('--num_classes', type=int, default=5)
+
+args = parser.parse_args()
+os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision as tv
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torch.utils.data import Dataset
+
+from dataset import ClassImageLoader
+from sampler import ImbalancedDatasetSampler
+from cunet import Conditional_UNet
+
+if __name__=='__main__':
+    os.makedirs(args.output_dir, exist_ok=True)
+    sep_data = pd.read_pickle(args.pkl_path)
+    print('loaded {} data'.format(len(sep_data['val'])))
+
+    transform = transforms.Compose([
+        transforms.Resize((args.input_size,)*2),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    dataset = ClassImageLoader(paths=sep_data['val'], transform=transform)
+    
+    loader = torch.utils.data.DataLoader(
+            dataset,
+            sampler=ImbalancedDatasetSampler(dataset),
+            batch_size=args.batch_size,
+            drop_last=True,
+            num_workers=args.num_workers)
+
+    #load model
+    classifer= torch.load(args.classifer_path)
+    classifer.eval()
+
+    if args.gpu > 0:
+        classifer.cuda()
+
+    bs = args.batch_size
+    s_li = ['sunny','cloudy', 'rain', 'snow', 'foggy'] 
+    
+    res_li = []
+    for i, data in tqdm(enumerate(loader), total=len(dataset)//bs):
+        batch = data[0].to('cuda')
+        c_batch = data[1].to('cuda')
+        pred = torch.argmax(classifer(batch).detach(), 1)
+        res_li.append(torch.cat([pred.int().cpu().view(bs,-1), c_batch.int().cpu().view(bs,-1)], 1))
+    all_res = torch.cat(cls_li, 0).numpy()
+    y_true, y_pred = (all_res[:, 0], all_res[:, 1])
+
+    table = classification_report(y_true, y_pred)
+
+    print(table)
+
+    matrix = confusion_matrix(y_true, y_pred, labels=np.arange(len(s_li)), normalize='pred')
+    df = pd.DataFrame(data=matrix, index=s_li, columns=s_li)
+
+    plot = sns.heatmap(df, square=True, annot=True, fmt='.3f')
+    fig = plot.get_figure()
+    fig.savefig(os.path.join(args.output_dir,'heatmap.png'))
