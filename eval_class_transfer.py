e8487d7b1ab929e2dc102c60b8325580bdc7b868
kgkgzrtk <r111331@g.kobe-kosen.ac.jp>
2020-02-13 16:19:32 +0900

    Update evaluation script

--- eval_class_transfer.py
+++ eval_class_transfer.py
@@ -15,7 +15,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int)
 parser.add_argument('--image_root', type=str, default='/mnt/fs2/2018/matsuzaki/dataset_fromnitta/Image/')
 parser.add_argument('--pkl_path', type=str, default='/mnt/data2/matsuzaki/repo/data/sepalated_data.pkl')
-parser.add_argument('--output_dir', type=str, default='/mnt/fs2/2018/matsuzaki/results/eval/classifer')
+parser.add_argument('--output_dir', type=str, default='/mnt/fs2/2018/matsuzaki/results/eval/transfer_class')
+parser.add_argument('--cp_path', type=str, default='/mnt/fs2/2018/matsuzaki/results/cp/transfer_class/i2w_res_aug_5_cls_n/i2w_res_aug_5_cls_n_e0026.pt')
 parser.add_argument('--classifer_path', type=str, default='cp/classifier/i2w_res101_val_n/resnet101_95.pt')
 parser.add_argument('--input_size', type=int, default=224)
 parser.add_argument('--batch_size', type=int, default=16)
@@ -39,8 +40,9 @@ from cunet import Conditional_UNet
 
 if __name__=='__main__':
     os.makedirs(args.output_dir, exist_ok=True)
-    sep_data = pd.read_pickle(args.pkl_path)
-    print('loaded {} data'.format(len(sep_data['val'])))
+    df = pd.read_pickle(args.pkl_path)
+    df = df['val']
+    print('loaded {} data'.format(len(df)))
 
     transform = transforms.Compose([
         transforms.Resize((args.input_size,)*2),
@@ -48,32 +50,50 @@ if __name__=='__main__':
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
 
-    dataset = ClassImageLoader(paths=sep_data['val'], transform=transform)
+    dataset = ClassImageLoader(paths=df, transform=transform)
     
     loader = torch.utils.data.DataLoader(
             dataset,
             sampler=ImbalancedDatasetSampler(dataset),
             batch_size=args.batch_size,
             drop_last=True,
-            num_workers=args.num_workers)
+            num_workers=args.num_workers
+            )
+    random_loader = torch.utils.data.DataLoader(
+            dataset,
+            sampler=ImbalancedDatasetSampler(dataset),
+            batch_size=args.batch_size,
+            drop_last=True,
+            num_workers=args.num_workers
+            )
 
     #load model
+    transfer = Conditional_UNet(num_classes=args.num_classes)
+    sd = torch.load(args.cp_path)
+    transfer.load_state_dict(sd['inference'])
+
     classifer= torch.load(args.classifer_path)
     classifer.eval()
 
     if args.gpu > 0:
+        transfer.cuda()
         classifer.cuda()
 
     bs = args.batch_size
     s_li = ['sunny','cloudy', 'rain', 'snow', 'foggy'] 
     
-    res_li = []
-    for i, data in tqdm(enumerate(loader), total=len(dataset)//bs):
+    cls_li = []
+    vec_li = []
+    for i, (data, rnd) in tqdm(enumerate(zip(loader, random_loader)), total=len(df)//bs):
         batch = data[0].to('cuda')
-        c_batch = data[1].to('cuda')
-        pred = torch.argmax(classifer(batch).detach(), 1)
-        res_li.append(torch.cat([pred.int().cpu().view(bs,-1), c_batch.int().cpu().view(bs,-1)], 1))
-    all_res = torch.cat(res_li, 0).numpy()
+        r_batch  = rnd[0].to('cuda')
+        c_batch = rnd[1].to('cuda')
+        c_batch = F.one_hot(c_batch, args.num_classes).float()
+        r_cls = torch.argmax(classifer(r_batch).detach(), 1)
+        out = transfer(batch, c_batch)
+        c_preds = torch.argmax(classifer(out).detach(), 1)
+        cls_li.append(torch.cat([r_cls.int().cpu().view(bs,-1), c_preds.int().cpu().view(bs,-1)], 1))
+    all_res = torch.cat(cls_li, 0).numpy()
     y_true, y_pred = (all_res[:, 0], all_res[:, 1])
 
     table = classification_report(y_true, y_pred)
--- eval_transfer.py
+++ eval_transfer.py
@@ -13,14 +13,16 @@ import seaborn as sns
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int)
-parser.add_argument('--image_root', type=str, default='/mnt/fs2/2018/matsuzaki/dataset_fromnitta/Image/')
-parser.add_argument('--pkl_path', type=str, default='/mnt/data2/matsuzaki/repo/data/sepalated_data.pkl')
-parser.add_argument('--output_dir', type=str, default='/mnt/fs2/2018/matsuzaki/results/eval/classifer')
+parser.add_argument('--pkl_path', type=str, default='/mnt/fs2/2018/matsuzaki/results/flickr_data/df_con_less25.pkl')
+parser.add_argument('--output_dir', type=str, default='/mnt/fs2/2018/matsuzaki/results/eval/transfer')
+parser.add_argument('--image_root', type=str, default='/mnt/fs2/2019/Takamuro/db/photos_usa_2016_outdoor')
+#parser.add_argument('--cp_path', type=str, default='/mnt/fs2/2018/matsuzaki/results/cp/out110_res101_e10_less25/out110_res101_e10_less25_e0015.pt')
+parser.add_argument('--cp_path', type=str, default='/mnt/fs2/2018/matsuzaki/results/cp/cunet_999/cunet_999_e0080.pt')
 parser.add_argument('--classifer_path', type=str, default='cp/classifier/i2w_res101_val_n/resnet101_95.pt')
 parser.add_argument('--input_size', type=int, default=224)
 parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--num_workers', type=int, default=4)
-parser.add_argument('--num_classes', type=int, default=5)
+parser.add_argument('--num_classes', type=int, default=6)
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
@@ -33,14 +35,17 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 from torch.utils.data import Dataset
 
-from dataset import ClassImageLoader
+from dataset import FlickrDataLoader
 from sampler import ImbalancedDatasetSampler
 from cunet import Conditional_UNet
 
 if __name__=='__main__':
     os.makedirs(args.output_dir, exist_ok=True)
-    sep_data = pd.read_pickle(args.pkl_path)
-    print('loaded {} data'.format(len(sep_data['val'])))
+    df = pd.read_pickle(args.pkl_path)
+    df = df[len(df)//2:]
+    df = df[:1000]
+    print('loaded {} data'.format(len(df)))
+    cols = ['clouds', 'temp', 'humidity', 'pressure', 'windspeed', 'rain']
 
     transform = transforms.Compose([
         transforms.Resize((args.input_size,)*2),
@@ -48,32 +53,50 @@ if __name__=='__main__':
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
 
-    dataset = ClassImageLoader(paths=sep_data['val'], transform=transform)
+    dataset = FlickrDataLoader(args.image_root, df, cols, transform=transform)
+    classes = dataset.cls_li
     
     loader = torch.utils.data.DataLoader(
             dataset,
             sampler=ImbalancedDatasetSampler(dataset),
             batch_size=args.batch_size,
             drop_last=True,
-            num_workers=args.num_workers)
+            num_workers=args.num_workers
+            )
+    random_loader = torch.utils.data.DataLoader(
+            dataset,
+            sampler=ImbalancedDatasetSampler(dataset),
+            batch_size=args.batch_size,
+            drop_last=True,
+            num_workers=args.num_workers
+            )
 
     #load model
+    transfer = Conditional_UNet(num_classes=args.num_classes)
+    sd = torch.load(args.cp_path)
+    transfer.load_state_dict(sd['inference'])
+
     classifer= torch.load(args.classifer_path)
     classifer.eval()
 
     if args.gpu > 0:
+        transfer.cuda()
         classifer.cuda()
 
     bs = args.batch_size
     s_li = ['sunny','cloudy', 'rain', 'snow', 'foggy'] 
     
-    res_li = []
-    for i, data in tqdm(enumerate(loader), total=len(dataset)//bs):
+    cls_li = []
+    vec_li = []
+    for i, (data, rnd) in tqdm(enumerate(zip(loader, random_loader)), total=len(df)//bs):
         batch = data[0].to('cuda')
-        c_batch = data[1].to('cuda')
-        pred = torch.argmax(classifer(batch).detach(), 1)
-        res_li.append(torch.cat([pred.int().cpu().view(bs,-1), c_batch.int().cpu().view(bs,-1)], 1))
-    all_res = torch.cat(res_li, 0).numpy()
+        r_batch  = rnd[0].to('cuda')
+        c_batch = rnd[1].to('cuda')
+        r_cls = torch.argmax(classifer(r_batch).detach(), 1)
+        out = transfer(batch, c_batch)
+        c_preds = torch.argmax(classifer(out).detach(), 1)
+        cls_li.append(torch.cat([r_cls.int().cpu().view(bs,-1), c_preds.int().cpu().view(bs,-1)], 1))
+    all_res = torch.cat(cls_li, 0).numpy()
     y_true, y_pred = (all_res[:, 0], all_res[:, 1])
 
     table = classification_report(y_true, y_pred)
